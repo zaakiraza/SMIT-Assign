6332456cca25004f0adfa4d20f95846dee979665
zaakiraza <raaza.zaakir@gmail.com>
2024-12-10 11:30:22 +0500

    All Assignments are done

--- JS/Ass #49-52/app.js
+++ JS/Ass #49-52/app.js
@@ -1,8 +1,8 @@
 // Question#1
 let formSubmit=document.querySelector('#formSubmit');
-let input1=document.getElementById('input1');
-let input2=document.getElementById('input2');
-let input3=document.getElementById('input3');
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+let input3 = document.getElementById('input3');
 
 formSubmit.addEventListener('click',()=>{
     alert("Form Submitted Successfully");
@@ -29,7 +29,28 @@ showLess=()=>{
 }
 
 // Question#3
-let dataShow=document.getElementById('dataShow');
-function fun(){
-    dataShow.innerText=`Name is ${input1.value} +  Email is ${input2.value} + Password is ${input3.value}`
+const fun2=(id_Name)=>{
+    let elem=document.getElementById(id_Name);
+    elem.remove();
+}
+
+let dataShow1 = document.getElementById('dataShow1');
+let dataShow2 = document.getElementById('dataShow2');
+let dataShow3 = document.getElementById('dataShow3');
+let count=1;
+let table = document.getElementById('table1');
+function fun() {
+    let rowId = `row${count}`;
+    table.innerHTML += `
+        <tr id="${rowId}">
+            <td>${input1.value}</td>
+            <td>${input2.value}</td>
+            <td>${input3.value}</td>
+            <td onclick="fun2('${rowId}')">delete</td>
+        </tr>
+    `;
+    input1.value = "";
+    input2.value = "";
+    input3.value = "";
+    count++;
 }
